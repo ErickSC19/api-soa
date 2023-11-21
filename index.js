8bd8b2364b3f42a3c45e20efad516f2d0e33e791
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-11-21 11:12:58 -0600

    chat socket added

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+import { createServer } from 'node:http';
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { Server } from 'socket.io';
 import connectDB from "./config/db.js";
 import companiesRoutes from "./routes/companiesRoutes.js";
 
 const app = express();
 app.use(express.json());
 dotenv.config();
+const server = createServer(app);
+const io = new Server(server);
 
 connectDB
   .authenticate()
@@ -31,6 +35,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use("/get-data", companiesRoutes);
+
+io.on('chat', (socket) => {
+  console.log('a user connected');
+  socket.on('message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 
